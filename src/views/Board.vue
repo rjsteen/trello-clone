ff71bd01abd0cfc19eaa6bc03ee03a2bb610ece6
Richard Steen <rsteen@covermymeds.com>
2020-06-16 20:38:00 -0500

    lesson 3

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,42 +1,49 @@
 <template>
   <div class="board">
     <div class="flex flex-row items-start">
-      <div
-        class="column"
-        v-for="(column, $columnIndex) of board.columns"
-        :key="$columnIndex"
-      >
-        <div class="flex items-center mb-2 font-bold">
-          {{ column.name }}
-        </div>
+      <div class="column" v-for="(column, $columnIndex) of board.columns" :key="$columnIndex">
+        <div class="flex items-center mb-2 font-bold">{{ column.name }}</div>
         <div class="list-reset">
           <div
             class="task"
             v-for="(task, $taskIndex) of column.tasks"
             :key="$taskIndex"
+            @click="goToTask(task)"
           >
-            <span class="w-full flex-no-shrink font-bold">
-              {{ task.name }}
-            </span>
+            <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
             <p
               v-if="task.description"
               class="w-full flex-no-shrink mt-1 text-sm"
-            >
-              {{ task.description }}
-            </p>
+            >{{ task.description }}</p>
           </div>
         </div>
+      </div>
+      <div class="task-bg" v-if="isTaskOpen" @click.self="close">
+        <router-view />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-  computed: mapState(['board'])
-}
+  computed: {
+    ...mapState(["board"]),
+    isTaskOpen() {
+      return this.$route.name === "task";
+    }
+  },
+  methods: {
+    goToTask(task) {
+      this.$router.push({ name: "task", params: { id: task.id } });
+    },
+    close() {
+      this.$router.push({ name: "board" });
+    }
+  }
+};
 </script>
 
 <style lang="css">
@@ -55,6 +62,6 @@ export default {
 
 .task-bg {
   @apply pin absolute;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
